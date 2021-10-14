ea7cb534399418484ba4ca2e83376cc7b13b0c87
Jay Patel <jaykumarpatel657@gmail.com>
2021-10-14 19:52:20 +0530

    build update

---
+++ frontend/build/precache-manifest.f25d39d4c1bc68fb82e03f3f5e91f2c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d28b4ff411a60e847c6ed60cbded79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf9a1226549fffa0bc0d",
+    "url": "/static/css/main.010fcd9a.chunk.css"
+  },
+  {
+    "revision": "56e9e603973d2666549d",
+    "url": "/static/js/2.e5bbb9d4.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.e5bbb9d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9a1226549fffa0bc0d",
+    "url": "/static/js/main.471060ce.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
